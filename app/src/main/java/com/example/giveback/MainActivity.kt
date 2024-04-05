e95dbc2e77f7c5991d68bdc977bde960aecfa1b6
ylose <heungeob1003@gmail.com>
2024-04-06 03:07:13 +0900

    24.04.06 우측 최상단 메뉴 버튼 삭제

--- app/src/main/java/com/example/giveback/MainActivity.kt
+++ app/src/main/java/com/example/giveback/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.example.giveback
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import com.example.giveback.R
-import com.example.giveback.setting.SettingActivity
 import com.google.firebase.auth.FirebaseAuth
 
 // 메인 화면
@@ -21,11 +17,5 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // 우측 최상단의 세팅버튼을 누르면 로그아웃,회원탈퇴가 있는 셋팅 페이지로 이동한다.
-        findViewById<ImageView>(R.id.settingBtn).setOnClickListener {
-            val intent = Intent(this, SettingActivity::class.java)
-            startActivity(intent)
-        }
     }
 }

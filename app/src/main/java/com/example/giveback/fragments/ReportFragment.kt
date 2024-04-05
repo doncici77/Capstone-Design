9666f01acea3b4d69e123baff60896b1ae81e17e
ylose <heungeob1003@gmail.com>
2024-04-05 20:07:14 +0900

    24.04.05 분실신고 페이지 추가

---
+++ app/src/main/java/com/example/giveback/fragments/ReportFragment.kt
@@ -0,0 +1,50 @@
+package com.example.giveback.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.giveback.R
+import com.example.giveback.databinding.FragmentReportBinding
+
+// 분실신고 페이지
+class ReportFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private lateinit var binding: FragmentReportBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_report, container, false)
+
+        binding.tipTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_talkFragment_to_tipFragment)
+        }
+
+        binding.homeTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_talkFragment_to_homeFragment)
+        }
+
+        binding.bookmarkTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_talkFragment_to_bookmarkFragment)
+        }
+
+        binding.storeTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_talkFragment_to_storeFragment)
+        }
+
+        return binding.root
+    }
+
+}

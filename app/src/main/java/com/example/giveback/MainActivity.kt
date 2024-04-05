ec7d425636c88dfee428d2eb5bf17518ffbbf2e5
ylose <heungeob1003@gmail.com>
2024-04-05 19:59:05 +0900

    24.04.05 메인 페이지 추가

--- app/src/main/java/com/example/giveback/MainActivity.kt
+++ app/src/main/java/com/example/giveback/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.giveback
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.example.giveback.R
+import com.example.giveback.setting.SettingActivity
+import com.google.firebase.auth.FirebaseAuth
 
+// 메인 화면
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        auth = FirebaseAuth.getInstance()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 우측 최상단의 세팅버튼을 누르면 로그아웃,회원탈퇴가 있는 셋팅 페이지로 이동한다.
+        findViewById<ImageView>(R.id.settingBtn).setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

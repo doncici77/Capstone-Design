dbbd12d435c9e69c265a492a9784ee47b7e17789
ylose <heungeob1003@gmail.com>
2024-04-08 01:50:06 +0900

    24.04.08 로그아웃, 회원탈퇴 기능 구현

--- app/src/main/java/com/example/giveback/fragments/MyFragment.kt
+++ app/src/main/java/com/example/giveback/fragments/MyFragment.kt
@@ -42,6 +42,31 @@ class MyFragment : Fragment() {
 
         binding.email.setText("사용자: ${email}")
 
+
+
+        // 로그아웃 버튼을 클릭했을 때 로그아웃이 진행되고 로그인 페이지로 이동
+        binding.logoutBtn.setOnClickListener {
+            auth.signOut()
+
+            // LoginActivity로 화면 이동
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+
+        // SIGNOUT 버튼을 클릭했을 때 회원탈퇴가 진행되고 로그인 페이지로 이동
+        binding.signoutBtn.setOnClickListener {
+
+            auth.currentUser?.delete()
+
+            // LoginActivity로 화면 이동
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            
+        }
+
+
         binding.tipTap.setOnClickListener {
             it.findNavController().navigate(R.id.action_myFragment_to_tipFragment)
         }

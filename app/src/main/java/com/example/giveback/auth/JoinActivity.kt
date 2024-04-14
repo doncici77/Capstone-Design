843181b3afcb155ddf2e3245e48267a1792de246
doncici77 <pkh2956@naver.com>
2024-04-14 17:24:51 +0900

    회원가입 화면 수정 1. 이메일, 비번 필드 아래 안내메세지 추가, 토스메세지 삭제 2. 조건에 맞게 작성해야만 이메일 인증 활성화 3. 이메일 인증 후 회원가입 가능

--- app/src/main/java/com/example/giveback/auth/JoinActivity.kt
+++ app/src/main/java/com/example/giveback/auth/JoinActivity.kt
@@ -1,9 +1,13 @@
 package com.example.giveback.auth
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.giveback.MainActivity
 import com.example.giveback.R
@@ -16,6 +20,9 @@ class JoinActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityJoinBinding
 
+    private var email_ok: Boolean = false
+    private var pw_ok: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,6 +31,67 @@ class JoinActivity : AppCompatActivity() {
 
         // 회원가입 버튼 초기 상태는 비활성화
         binding.joinBtn.isEnabled = false
+
+        // 모든 필드 입력 되면 가입하기 버튼 활성화
+        // 1. 이메일 필드
+        binding.emailArea.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val email = s.toString()
+                email_ok = email.endsWith("@gwnu.ac.kr") || email.endsWith("@gwnu.mypiug.kr")
+                if (!email.contains('@')) {
+                    // 이메일에 '@'가 포함되어 있는지 확인
+                    binding.emailokText.setTextColor(Color.RED)
+                    binding.emailokText.text = "이메일 형식에 맞게 작성하세요."
+                } else if (!email_ok) {
+                    // 지정된 도메인 확인
+                    binding.emailokText.setTextColor(Color.RED)
+                    binding.emailokText.text = "gwnu.ac.kr 혹은 gwnu.mypiug.kr 도메인으로 입력하세요."
+                } else {
+                    binding.emailokText.setTextColor(ContextCompat.getColor(applicationContext, R.color.green))
+                    binding.emailokText.text = "유효한 아이디"
+                }
+                changeSignUpButtonActivation()
+            }
+        })
+
+        // 2. 비밀번호 필드
+        binding.passwordArea1.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                pw_ok = s?.length!! >= 6 && checkPassword() // 6자 이상 + 비밀번호 입력 정상
+                if (s?.length!! >= 6) {
+                    binding.pwokText.setTextColor(ContextCompat.getColor(applicationContext, R.color.green))
+                    binding.pwokText.text = "유효한 비밀번호"
+                } else {
+                    binding.pwokText.setTextColor(Color.RED)
+                    binding.pwokText.text = "비밀번호는 6자 이상이어야 합니다"
+                }
+                changeSignUpButtonActivation()
+            }
+        })
+
+        // 4. 비밀번호 확인 필드
+        binding.passwordArea2.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                pw_ok = s?.length!! >= 6 && checkPassword() // 6자 이상 + 비밀번호 입력 정상
+                if (pw_ok) {
+                    binding.pwcheckokText.setTextColor(ContextCompat.getColor(applicationContext, R.color.green))
+                    binding.pwcheckokText.text = "유효한 비밀번호 확인"
+                } else {
+                    binding.pwcheckokText.setTextColor(Color.RED)
+                    binding.pwcheckokText.text = "비밀번호가 위와 같아야 합니다"
+                }
+                changeSignUpButtonActivation()
+            }
+        })
 
         // 이메일 인증 버튼 클릭 리스너
         binding.verifyEmailBtn.setOnClickListener {
@@ -50,6 +118,16 @@ class JoinActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    // 이메일 인증 버튼 활성화 상태 변경 메소드
+    fun changeSignUpButtonActivation() : Unit {
+        binding.verifyEmailBtn.isEnabled =  email_ok == true && pw_ok == true
+    }
+
+    // 비밀번호 == 비밀번호 확인이면 true, 아니면 false
+    fun checkPassword() : Boolean {
+        return binding.passwordArea1.text.toString() == binding.passwordArea2.text.toString()
     }
 
     private fun createUserAndSendEmailVerification(email: String, password: String) {

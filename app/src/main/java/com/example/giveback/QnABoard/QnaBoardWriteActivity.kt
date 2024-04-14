5c3b9e02e8de808b7d5303dd1d905ce0c5d020b4
ylose <heungeob1003@gmail.com>
2024-04-15 00:40:46 +0900

    24.04.15 데이터 모델 값 수정

--- app/src/main/java/com/example/giveback/QnABoard/QnaBoardWriteActivity.kt
+++ app/src/main/java/com/example/giveback/QnABoard/QnaBoardWriteActivity.kt
@@ -8,11 +8,16 @@ import com.example.giveback.R
 import com.example.giveback.databinding.ActivityQnaBoardWriteBinding
 import com.example.giveback.utils.FBAuth
 import com.example.giveback.utils.FBRef
+import com.google.firebase.auth.FirebaseAuth
 
 // QnA 게시글 작성 페이지
 class QnaBoardWriteActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityQnaBoardWriteBinding
+
+    var auth = FirebaseAuth.getInstance()
+
+    var email = auth.currentUser?.email
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -29,16 +34,13 @@ class QnaBoardWriteActivity : AppCompatActivity() {
             // uid를 가져온다.
             val uid = FBAuth.getUid()
 
-            // time을 가져온다.
-            val time = FBAuth.getTime()
-
             // 키부터 생성하고 데이터베이스에 저장하도록 수정
             val key = FBRef.qnaboardRef.push().key.toString()
 
             // 파이어 베이스에 데이터를 저장한다.
             FBRef.qnaboardRef
                 .child(key) // 랜덤한 값
-                .setValue(QnaBoardModel(title,content,uid,time))
+                .setValue(QnaBoardModel(uid, email.toString(),title,content, "대기"))
 
             Toast.makeText(this,"게시글 입력 완료", Toast.LENGTH_LONG).show()
 

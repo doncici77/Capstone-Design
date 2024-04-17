748fab6326ff9b7a01183b03ecbe6470a7ff68a6
ylose <heungeob1003@gmail.com>
2024-04-17 21:11:14 +0900

    24.04.17 키워드 설정 시 설정한 사람의 uid 데이터모델 추가

--- app/src/main/java/com/example/giveback/Keyword/KeywordStatusModel.kt
+++ app/src/main/java/com/example/giveback/Keyword/KeywordStatusModel.kt
@@ -2,6 +2,7 @@ package com.example.giveback.QnABoard
 
 // 게시글의 데이터 구조를 정의
 data class KeywordStatusModel (
+    val uid: String ="", // 키워드 설정한 사람의 uid
     val category:String, // category
     val status:Boolean // keyword의 상태(on, off)
 )

f1074ad1479f6c8efdcac79a081adf6738b2cb2e
ylose <heungeob1003@gmail.com>
2024-04-30 16:21:27 +0900

    24.04.29 첫 번째로 선택한 이미지가 대표이미지가 되도록 수정

--- app/src/main/java/com/example/giveback/GetBoard/GetBoardListLVAdapter.kt
+++ app/src/main/java/com/example/giveback/GetBoard/GetBoardListLVAdapter.kt
@@ -42,7 +42,7 @@ class GetBoardListLVAdapter(val boardList : MutableList<GetBoardModel>, val boar
 
         val imageViewFromFB = view?.findViewById<ImageView>(R.id.imageArea)
 
-        val storageReference = Firebase.storage.reference.child("${boardKeyList[position]}1.png")
+        val storageReference = Firebase.storage.reference.child("${boardKeyList[position]}0.png")
 
         storageReference.downloadUrl.addOnCompleteListener { task ->
             if (task.isSuccessful) {

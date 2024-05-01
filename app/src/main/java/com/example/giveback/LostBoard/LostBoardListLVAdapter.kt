792afd147bfab8a530d142de7622bed7fbbe9058
ylose <heungeob1003@gmail.com>
2024-05-01 14:34:19 +0900

    24.05.01 분실물 리스트 어댑터 추가

--- app/src/main/java/com/example/giveback/LostBoard/LostBoardListLVAdapter.kt
+++ app/src/main/java/com/example/giveback/LostBoard/LostBoardListLVAdapter.kt
@@ -5,13 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
 import com.example.giveback.R
 import com.example.giveback.utils.FBAuth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 // 리스트뷰와 게시글 데이터를 연결해주는 게시물 리스트 어댑터
-class LostBoardListLVAdapter(val boardList : MutableList<LostBoardModel>): BaseAdapter() {
+class LostBoardListLVAdapter(val boardList : MutableList<LostBoardModel>, val boardKeyList: MutableList<String>): BaseAdapter() {
     override fun getCount(): Int {
         return boardList.size
     }
@@ -31,23 +36,39 @@ class LostBoardListLVAdapter(val boardList : MutableList<LostBoardModel>): BaseA
 
         //if(view== null) {
 
-            view = LayoutInflater.from(parent?.context).inflate(R.layout.board_list_item, parent,false)
+        view = LayoutInflater.from(parent?.context).inflate(R.layout.lost_board_list_item, parent,false)
         //}
 
+        val imageViewFromFB = view?.findViewById<ImageView>(R.id.imageArea)
+
+        val storageReference = Firebase.storage.reference.child("${boardKeyList[position]}0.png")
+
+        storageReference.downloadUrl.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val downloadUrl = task.result
+                if (imageViewFromFB != null) {
+                    Glide.with(view)
+                        .load(downloadUrl)
+                        .into(imageViewFromFB)
+                }
+            } else {
+                imageViewFromFB?.isVisible = false
+            }
+        }
+
         val title = view?.findViewById<TextView>(R.id.titleArea)
-        val content = view?.findViewById<TextView>(R.id.contentArea)
-        val time = view?.findViewById<TextView>(R.id.timeArea)
+        val LostLocation = view?.findViewById<TextView>(R.id.lostlocationArea)
+        val LostDate = view?.findViewById<TextView>(R.id.lostDateArea)
 
         val itemLinearLayoutView = view?.findViewById<LinearLayout>(R.id.itemView)
         if(boardList[position].uid.equals(FBAuth.getUid())) {
-            itemLinearLayoutView?.setBackgroundColor(Color.parseColor("#ffa500"))
+            itemLinearLayoutView?.setBackgroundColor(Color.parseColor("#EEEEEE"))
         }
 
-        title!!.text = boardList[position].title
-        content!!.text = boardList[position].content
-        time!!.text = boardList[position].time
+        title!!.text = "분실물명: ${boardList[position].title}"
+        LostLocation!!.text = "분실장소: ${boardList[position].lostLocation} ${boardList[position].lostdetailLocation}"
+        LostDate!!.text = boardList[position].lostDate
 
         return view!!
     }
-
 }
